0f5a12d886e427f0330282befebd002b6b8d9b27
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-29 13:03:34 +0500

    Update and rename love.js to 0_🤞RDX _love.js

--- models/commands/0_🤞RDX _love.js
+++ models/commands/0_🤞RDX _love.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "love",
     version: "2.6.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "",
     commandCategory: "Love",
     usages: "[tag]",

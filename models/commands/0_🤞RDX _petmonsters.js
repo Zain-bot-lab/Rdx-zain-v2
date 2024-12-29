6c0864843580492c4ca7722762dfc7c70d2837c2
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-29 16:53:32 +0500

    Update and rename petmonsters.js to 0_🤞RDX _petmonsters.js

--- models/commands/0_🤞RDX _petmonsters.js
+++ models/commands/0_🤞RDX _petmonsters.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "petmonsters",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "Sun bears are alone together :))",
     commandCategory: "game",
     usages: "-r/-s/-l/-p",

0b751b481392d782f99ed484d8ecf2d06ba50156
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-29 13:08:51 +0500

    Update and rename marriedv4.js to 0_🤞RDX _marriedv4.js

--- models/commands/0_🤞RDX _marriedv4.js
+++ models/commands/0_🤞RDX _marriedv4.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "marriedv4",
     version: "3.1.1",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "married",
     commandCategory: "img",
     usages: "[@mention]",

c79b6e5b7f367640c722b0ec1e55979aac17d484
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-29 13:28:35 +0500

    Update and rename pair5.js to 0_🤞RDX _pair5.js

--- models/commands/0_🤞RDX _pair5.js
+++ models/commands/0_🤞RDX _pair5.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "pair5",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "It's a compound :>",
   commandCategory: "fun",
   usages: "",

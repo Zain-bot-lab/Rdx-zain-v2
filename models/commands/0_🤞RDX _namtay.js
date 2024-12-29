fa03c5bb67e1da9714421a9e46ba4a7f049f0a09
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-29 13:16:30 +0500

    Update and rename namtay.js to 0_🤞RDX _namtay.js

--- models/commands/0_🤞RDX _namtay.js
+++ models/commands/0_🤞RDX _namtay.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "hand",
     version: "2.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "",
     commandCategory: "Love",
     usages: "[tag]",

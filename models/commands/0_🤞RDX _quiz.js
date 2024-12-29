842af4e7fd8845a8d005cfde177f067cb4cb40e8
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-29 16:58:55 +0500

    Update and rename quiz.js to 0_🤞RDX _quiz.js

--- models/commands/0_🤞RDX _quiz.js
+++ models/commands/0_🤞RDX _quiz.js
@@ -1,7 +1,7 @@
 module.exports.config = {
 	name: "quiz",
 	version: "1.0.0",
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	hasPermssion: 0,
 	description: "Answer questions (English)",
 	commandCategory: "game",

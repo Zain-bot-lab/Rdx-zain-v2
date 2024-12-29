71a2cee1ad3a053b46a4b12e7a54dd96754c230a
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-29 17:07:15 +0500

    Update and rename selfadmin.js to 0_🤞RDX _selfadmin.js

--- models/commands/0_🤞RDX _selfadmin.js
+++ models/commands/0_🤞RDX _selfadmin.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "self",
 	version: "1.0.5",
 	hasPermssion: 0, 
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝐏RDX_ZAIN",
 	description: "Manage bot admin",
 	commandCategory: "config",
 	usages: "[list/add/remove] [userID]",

29eb2dcd9bf970c9db561f5b83b93b8a5b5e8ab1
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-29 16:54:41 +0500

    Update ping.js

--- models/commands/ping.js
+++ models/commands/ping.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "ping",
 	version: "1.0.5",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Tag all members",
 	commandCategory: "system",
 	usages: "[Text]",

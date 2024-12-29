8080aacd72ab0d17e24d071deba94147002a2d3f
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-29 13:14:31 +0500

    Update and rename mlstalk.js to 0_🤞RDX _mlstalk.js

--- models/commands/0_🤞RDX _mlstalk.js
+++ models/commands/0_🤞RDX _mlstalk.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "mlstalk",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "morse code to text",
   usages: "[id | server]",
 	commandCategory: "...",

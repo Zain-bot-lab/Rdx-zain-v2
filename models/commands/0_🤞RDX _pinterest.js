047dae7216fc2631a054d414ce05144298f9a2e3
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-29 16:55:48 +0500

    Update and rename pinterest.js to 0_🤞RDX _pinterest.js

--- models/commands/0_🤞RDX _pinterest.js
+++ models/commands/0_🤞RDX _pinterest.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "pinterest",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "RDX_ZAIN",
     description: "Image search",
     commandCategory: "Search",
     usePrefix: false,

2aae47e28c01879298f3110844b561e6c245088d
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-29 17:06:06 +0500

    Update and rename schoolid.js to 0_🤞RDX _schoolid.js

--- models/commands/0_🤞RDX _schoolid.js
+++ models/commands/0_🤞RDX _schoolid.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "schoolid",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RDX_ZAIN",
   description: "Fake School Id",
   commandCategory: "image",
   usages: "@mention",

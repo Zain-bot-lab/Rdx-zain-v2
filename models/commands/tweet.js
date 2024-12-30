07327af88c95a892d0b4c4b7f4172e38d57155a7
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-30 18:18:26 +0500

    Update tweet.js

--- models/commands/tweet.js
+++ models/commands/tweet.js
@@ -4,7 +4,7 @@ module.exports.config = {
  
         version: "1.0.1",
         hasPermssion: 0,
-        credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+        credits: "RDX_ZAIN",
         description: "",
         commandCategory: "edit-img",
         usages: "[text]",

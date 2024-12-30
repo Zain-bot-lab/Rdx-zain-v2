dbf0296e78d8831c95af08eb2004325e1a115b46
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-30 18:28:27 +0500

    Update and rename women.js to 0_🤞RDX _women.js

--- models/commands/0_🤞RDX _women.js
+++ models/commands/0_🤞RDX _women.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "women",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+	credits: "RDX-ZAIN", 
 	description: "hihihihi",
 	commandCategory: "no prefix",
 	usages: "women",

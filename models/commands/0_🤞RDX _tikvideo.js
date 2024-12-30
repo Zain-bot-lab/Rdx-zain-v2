40998ddd65802bea4547177b9c82501ca686bd26
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-30 18:14:46 +0500

    Update and rename tikvideo.js to 0_🤞RDX _tikvideo.js

--- models/commands/0_🤞RDX _tikvideo.js
+++ models/commands/0_🤞RDX _tikvideo.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "tikvideo",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Download tiktok video",
 	commandCategory: "For user",
 	usages: "",

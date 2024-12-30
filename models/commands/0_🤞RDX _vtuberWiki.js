8b6f0cb60ed310e5faa45594b519cd8365f22608
ZAIN PRINCE <8ballpoolpaas@gmail.com>
2024-12-30 18:25:04 +0500

    Update and rename vtuberWiki.js to 0_🤞RDX _vtuberWiki.js

--- models/commands/0_🤞RDX _vtuberWiki.js
+++ models/commands/0_🤞RDX _vtuberWiki.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "vtuber_wiki",
 	version: "2.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "RDX_ZAIN",
 	description: "Search VTubers on VTubersWiki Official",
 	commandCategory: "vtuber",
 	usages: "vtuber_wiki [vtuber name]",
